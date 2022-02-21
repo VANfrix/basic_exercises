5049dd0ad2d7187ac071cf56b873cabedc49f02b
VANfrix <ultrafrost2012@yandex.ru>
2022-02-21 18:12:39 +0300

    Доп ДЗ

--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,34 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
 # Вывести количество букв "а" в слове
-word = 'Архангельск'
-# ???
+# word = 'Архангельск'
+# leter = word.len()
+# print(leter)
 
 
 # Вывести количество гласных букв в слове
-word = 'Архангельск'
-# ???
+# word = 'Архангельск'
+# print(len(word()
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+# print(len(sentence.split()))
 
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+
+for leter in sentence:
+    print(leter.lower())
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+sred_1 = (sentence.split())
+sred_2 = (len(sred_1[0]) +len(sred_1[1]) +len(sred_1[2]) +len(sred_1[3]))/len(sred_1)
+# print(sred_1)
+print(int(sred_2))
